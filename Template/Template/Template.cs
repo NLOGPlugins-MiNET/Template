5583ecacf1fd1462cba6c080b50a0d98450564cb
Herb9 <pksxtm@gmail.com>
2017-08-05 07:22:57 +0900

    Initial commit

---
+++ Template/Template/Template.cs
@@ -0,0 +1,34 @@
+﻿
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+
+/*
+    ########  #### ########    ###    
+    ##     ##  ##  ##         ## ##   
+    ##     ##  ##  ##        ##   ##  
+    ########   ##  ######   ##     ## 
+    ##         ##  ##       ######### 
+    ##         ##  ##       ##     ## 
+    ##        #### ######## ##     ## 
+    
+    PIEA, MINET Developers' Organization.
+*/
+
+namespace Template
+{
+
+    [Plugin(PluginName = "Template", Description = "PIEA plugin basic template.", PluginVersion = "None", Author = "PIEA")]
+    public class Template : Plugin
+    {
+
+        protected override void OnEnable()
+        {
+           
+        }
+
+        public override void OnDisable()
+        {
+            
+        }
+    }
+}
