da6f3c8e6172dddad0f55ec2c77a077b710315b7
Herb9 <pksxtm@gmail.com>
2017-08-06 22:05:04 +0900

    Eliminated futile space.

--- Template/Template/Template.cs
+++ Template/Template/Template.cs
@@ -11,16 +11,14 @@ using MiNET.Plugins.Attributes;
     ##         ##  ##       ##     ## 
     ##        #### ######## ##     ## 
     
-    PIEA, MINET Developers' Organization.
+    PIEA, MINET Developers' Team.
 */
 
 namespace Template
 {
-
     [Plugin(PluginName = "Template", Description = "PIEA plugin basic template.", PluginVersion = "None", Author = "PIEA")]
     public class Template : Plugin
     {
-
         protected override void OnEnable()
         {
            
